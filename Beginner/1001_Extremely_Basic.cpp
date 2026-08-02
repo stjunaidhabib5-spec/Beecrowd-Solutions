770f28287bfcee4591bb216b5f96d865289f93dd
Md. Junaid Al Habib Siam <stjunaidhabib5@gmail.com>
2026-08-02 23:02:43 +0600

    Add 1001 and 1004 solutions

---
+++ Beginner/1001_Extremely_Basic.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std ;
+
+int main ()
+{
+    int A ;
+    int B ;
+    cin>>A ;
+    cin>>B ;
+    
+    int X = A+B ;
+    cout<< "X = "<<X<<endl ;
+    return 0 ; 
+}
---
+++ Beginner/1004_simple_product.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std ;
+int main (){
+    int a ;
+    int b ;
+    cin >> a;
+    cin >> b;
+    int PROD = a*b ;
+    cout <<"PROD = "<<PROD<<endl;
+    return 0 ;
+}
